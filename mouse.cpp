a6495e5706cc305efb7205e524f9ebd12ae0ac11
Tuấn Nghĩa Phạm <nghiamkqn@gmail.com>
2022-03-01 01:43:00 +0700

    mouse

---
+++ mouse.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+//#include <SDL.h>
+#include <SDL.h>
+
+using namespace std;
+
+//**************************************************************
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "Mouse in SDL";
+
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal = false);
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+void waitUntilKeyPressed();
+//**************************************************************
+
+void refreshScreen(SDL_Window* window, SDL_Renderer* renderer,
+    const SDL_Rect& filled_rect, const bool leftMouse = false);
+
+int main(int argc, char* argv[])
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    initSDL(window, renderer);
+
+    // Your code here
+    SDL_Event e;
+
+    // Định nghĩa toạ độ ban đầu và kích cỡ hình chữ nhật
+    SDL_Rect filled_rect;
+    filled_rect.x = SCREEN_WIDTH / 2;
+    filled_rect.y = SCREEN_HEIGHT / 2;
+    filled_rect.w = 16;
+    filled_rect.h = 12;
+    // Xoá toàn bộ màn hình và vẽ
+    refreshScreen(window, renderer, filled_rect);
+
+    while (true) {
+        // Đợi 10 mili giây
+        SDL_Delay(10);
+
+        // Nếu không có sự kiện gì thì tiếp tục trở về đầu vòng lặp
+        if ( SDL_WaitEvent(&e) == 0) continue;
+
+        // Nếu sự kiện là kết thúc (như đóng cửa sổ) thì thoát khỏi vòng lặp
+        if (e.type == SDL_QUIT) break;
+
+        // Nếu nhấn phìm ESC thì thoát khỏi vòng lặp
+        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) break;
+
+        // Nếu chuột (mouse) được nhấn (xuống)
+        if (e.type == SDL_MOUSEBUTTONDOWN) {
+            filled_rect.x = e.button.x; // Lấy hoành độ x của chuột
+            filled_rect.y = e.button.y; // Lấy tung độ y của chuột
+            // Xoá toàn bộ màn hình và vẽ lại với màu sắc tuỳ thuộc chuột trái hay phải được nhấn
+            if (e.button.button == SDL_BUTTON_LEFT)
+                refreshScreen(window, renderer, filled_rect, true);
+            else
+                refreshScreen(window, renderer, filled_rect, false);
+        }
+    }
+
+    quitSDL(window, renderer);
+    return 0;
+}
+
+// Xoá và vẽ lại toàn bộ màn hình với 1 hình chữ nhật
+void refreshScreen(SDL_Window* window, SDL_Renderer* renderer,
+    const SDL_Rect& filled_rect, const bool leftMouse)
+{
+    // Đặt màu vẽ thành xanh lam (blue), xoá màn hình về màu xanh lam.
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);   // blue
+    SDL_RenderClear(renderer);
+
+    // Vẽ hình chữ nhật với màu tuỳ thuộc nút chuột trái hay phải được nhấn
+    if (leftMouse)
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);   // red
+    else
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);   // green
+    SDL_RenderFillRect(renderer, &filled_rect);
+
+    // Dùng lệnh hiển thị (đưa) hình đã vẽ ra mành hình
+   //Khi thông thường chạy với môi trường bình thường ở nhà
+    SDL_RenderPresent(renderer);
+   //Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
+   //SDL_UpdateWindowSurface(window);
+}
+
+
+//*****************************************************
+// Các hàm chung về khởi tạo và huỷ SDL
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+       SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    //window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+    //   SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+
+    //Khi thông thường chạy với môi trường bình thường ở nhà
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+    //Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
+    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if ( SDL_WaitEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
+}
+//**************************************************************
+
